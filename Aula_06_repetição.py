f7fc08b8529f938e2a70d13d658d453b1201e03d
jeniffer-schwengber <160552556+jeniffer-schwengber@users.noreply.github.com>
2024-04-01 21:18:43 -0300

    Add files via upload

---
+++ Aula_06_repetição.py
@@ -0,0 +1,55 @@
+#contador= 0
+#while contador <= 10:
+   # nome= str(input("Digite seu nome:"))
+    #contador=contador +1
+
+# numero_digitado =0
+# while numero_digitado != 99:
+#     numero_digitado = int(input("Digite um numero qualquer e 99 para sair:"))
+#     print("O numero digitado foi: {}".format(numero_digitado))
+# print("Programa encerrado")
+# print("Programa gerador de tabuada")
+# numero = int(input("Digite um numero qualquer"))
+# print("Tabuada do numero {}".format(numero))
+# multiplicador= 0
+# while multiplicador<=10 +1
+
+# print("Programa gerador de Tabuada")
+# numero = int(input("Digite um numero qualquer"))
+# multiplicador = 0
+# while multiplicador <= 10:
+#     resultado = numero*multiplicador
+#     print("{} * {}= {}" .format(numero, multiplicador, resultado))
+#     multiplicador = multiplicador + 1
+
+# numero = int(input("Digite um numero maior que 1:"))
+# contador = 2
+# while contador >= numero:
+#     print("Os numeros pares são:{}".format(contador))
+#     contador = contador + 2
+
+# numero = int(input("Digite um numero maior que 0:"))
+# contador = 0
+# while contador <= numero:
+#     if contador % 2 == 0:
+#         print("Numeros pares: {}".format(contador))
+#         contador = contador +1
+
+"""
+Peça um numero qualquer para o usuario e imprima todos os numeros que o antecede até o 0
+
+# """
+#     numero = int(input('Digite um numero: '))
+#     while numero >=0:
+# antecessor = numero - 1<= 0
+#     print(f'O antecessor de seu numero é {antecessor}')
+
+
+lista_numeros = []
+contador  = 0
+while contador < 5:
+ numero = int(input("Informe um numero inteiro qualquer"))
+ lista_numeros.append(numero)
+ contador = contador + 1
+soma = sum(lista_numeros)
+print("A soma dos numeros é igual a :{}".format(soma))
